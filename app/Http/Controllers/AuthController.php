e7d0476434836e390cb369de2f5c6e835bbc623b
Kati <kati@smylee.com.au>
2023-05-09 11:27:46 +0300

    in progress

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,63 +9,42 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-    // Show the login form
-    public function showLoginForm()
-    {
-        return view('auth.login');
+    public function login() {
+        // Input constraint
+        // username must be unique, min length of 6 , max length of 30, and is required
+        // password is required, min length of 6 and max length of 30
+        // password confirmation is required and must be same as password field
+        // show error whenever needed if any of this constraint got violated
+        return view('login');
+
     }
 
-    // Handle login form submission
-    public function login(Request $request)
-{
-    // Validate the user input
-    $this->validate($request, [
-        'username' => 'required',
-        'password' => 'required',
-    ]);
-
-    // Attempt to log the user in
-    if (Auth::attempt(['username' => $request->input('username'), 'password' => $request->input('password')])) {
-        // If login successful, check if the user has an active session
-        if (Auth::check()) {
-            // If the user has an active session, redirect them to the home page
-            return redirect('/home');
-        } else {
-            // If the user doesn't have an active session, redirect them to the end page
-            return redirect('/end');
+    public function register(Request $request) {
+        
+        // Input constraint:
+        // -username must be unique, min length of 6 , max length of 30, and is required
+        // -password is required, min length of 6 and max length of 30
+        // -password confirmation is required and must be same as password field
+        // If all constraint is satisfied, hash the password
+        // store the password and the username in users table.
+        // show error whenever needed if any of this constraint got violated
+        {
+            // Validate the user input
+            $this->validate($request, [
+                'username' => 'required|unique:users|max:30|min:6',
+                'password' => 'required|max:30|min:6|confirmed',
+            ]);
+    
+            // Create a new user record
+            $user = new User;
+            $user->username = $request->input('username');
+            $user->password = Hash::make($request->input('password'));
+            $user->save();
+    
+            // Log the user in
+            Auth::login($user);
+    
+            return view('register');
         }
-    } else {
-        // If login fails, redirect back to the login form with an error message
-        return redirect('/login')->with('error', 'Invalid username or password');
-    }
-}
-
-
-    // Show the registration form
-    public function showRegistrationForm()
-    {
-        return view('auth.register');
-    }
-
-    // Handle registration form submission
-    public function register(Request $request)
-    {
-        // Validate the user input
-        $this->validate($request, [
-            'username' => 'required|unique:users|max:30|min:6',
-            'password' => 'required|max:30|min:6|confirmed',
-        ]);
-
-        // Create a new user record
-        $user = new User;
-        $user->username = $request->input('username');
-        $user->password = Hash::make($request->input('password'));
-        $user->save();
-
-        // Log the user in
-        Auth::login($user);
-
-        // Redirect to the home page
-        return redirect('/home');
     }
 }
